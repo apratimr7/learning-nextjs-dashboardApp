b983e7580c1e4f406667f8cb31bb54b5d3d92463
Apratim Rastogi <63489363+apratimr7@users.noreply.github.com>
2025-09-13 21:00:16 +0530

    Minor fixes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+// This file is to keep the fonts used throughout the website.
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: "400", 
+  style: "normal", 
+  subsets: ['latin'] 
+});
